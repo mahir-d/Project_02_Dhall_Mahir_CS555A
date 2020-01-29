6bd87fcb4898b4fcfba33ec7c95c2e2476861d7e
Mahir <mdhall@stevens.edu>
2020-01-29 18:50:49 -0500

    added gedcom file

---
+++ GedcomValidator.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.util.Scanner;
+public class GedcomValidator {
+
+
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner("/Users/mahirdhall/Desktop/Project_02_Dhall_Mahir_CS555A/My-Family-27-Jan-2020-330.ged")
+        
+    }
+
+}
