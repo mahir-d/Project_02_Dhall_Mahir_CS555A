70e2ee652b4a3c35f10ee5c3f4bd9e22cd8c1045
Mahir <mdhall@stevens.edu>
2020-01-30 16:02:36 -0500

    added File reader and scanner

--- GedcomValidator.java
+++ GedcomValidator.java
@@ -1,12 +1,21 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+
 public class GedcomValidator {
 
-
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
+       
+        File file = new File("/Users/mahirdhall/Desktop/Project_02_Dhall_Mahir_CS555A/My-Family-27-Jan-2020-330.ged");
         
-        Scanner sc = new Scanner("/Users/mahirdhall/Desktop/Project_02_Dhall_Mahir_CS555A/My-Family-27-Jan-2020-330.ged")
+        Scanner sc = new Scanner(file);
+        String fileContent = "";
+        while (sc.hasNextLine()) {
+            fileContent = fileContent.concat(sc.nextLine() + "\n");
+            System.out.println(sc.nextLine());
+        }
         
+        sc.close();
     }
 
 }
