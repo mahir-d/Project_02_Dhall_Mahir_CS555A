d2aa3ff045d8de1301db8b94e5dae855f52f150a
Mahir <mdhall@stevens.edu>
2020-02-02 14:38:30 -0500

    added file writer and removed space from the input line console

--- GedcomValidator.java
+++ GedcomValidator.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -7,7 +8,12 @@ public class GedcomValidator {
 
     public static void main(String args[]) throws IOException {
 
+        //Input test file provided
         File file = new File("/Users/mahirdhall/Desktop/Project_02_Dhall_Mahir_CS555A/proj02test.ged");
+
+
+        //My custom family
+        //File file = new File("/Users/mahirdhall/Desktop/Project_02_Dhall_Mahir_CS555A/My-Family-27-Jan-2020-330.ged");
 
         Scanner sc = new Scanner(file);
 
@@ -32,11 +38,13 @@ public class GedcomValidator {
 
         tags.put("DATE", "2");
 
+        //FileWriter writer = new FileWriter("/Users/mahirdhall/Desktop/customFamilOutput.txt");
+        FileWriter writer = new FileWriter("/Users/mahirdhall/Desktop/inputTestFileOutput.txt");
         while (sc.hasNextLine()) {
             String currLine = sc.nextLine();
 
-            System.out.println("--> " + currLine);
-
+            System.out.println("-->" + currLine);
+            writer.write("-->" + currLine+"\n");
             String[] currArr = currLine.split(" ");
 
             String level = currArr[0];
@@ -44,14 +52,19 @@ public class GedcomValidator {
             if (level.equals("0")) {
                 if (currArr[1].equals("HEAD") || currArr[1].equals("TRLR") || currArr[1].equals("NOTE")) {
                     System.out.print("<--" + level + "|" + currArr[1] + "|Y|");
+                    writer.write("<--" + level + "|" + currArr[1] + "|Y|");
                     for (int i = 2; i < currArr.length; i++) {
+                        writer.write(currArr[i] + " ");
                         System.out.print(currArr[i] + " ");
                     }
                 } else if (currArr[2].equals("INDI") || currArr[2].equals("FAM")) {
+                    writer.write("<--" + level + "|" + currArr[2] + "|Y|" + currArr[1]);
                     System.out.print("<--" + level + "|" + currArr[2] + "|Y|" + currArr[1]);
                 } else {
+                    writer.write("<--" + level + "|" + currArr[1] + "|N|");
                     System.out.print("<--" + level + "|" + currArr[1] + "|N|");
                     for (int i = 2; i < currArr.length; i++) {
+                        writer.write(currArr[i] + " ");
                         System.out.print(currArr[i] + " ");
                     }
                 }
@@ -61,47 +74,60 @@ public class GedcomValidator {
             else if (level.equals("1")) {
                 if (tags.containsKey(currArr[1])) {
                     if (tags.get(currArr[1]).equals(level)) {
+                        writer.write("<--" + level + "|" + currArr[1] + "|Y|");
                         System.out.print("<--" + level + "|" + currArr[1] + "|Y|");
                         for (int i = 2; i < currArr.length; i++) {
                             System.out.print(currArr[i] + " ");
+                            writer.write(currArr[i] + " ");
                         }
                     } else {
+                        writer.write("<--" + level + "|" + currArr[1] + "|N|");
                         System.out.print("<--" + level + "|" + currArr[1] + "|N|");
                         for (int i = 2; i < currArr.length; i++) {
+                            writer.write(currArr[i] + " ");
                             System.out.print(currArr[i] + " ");
                         }
                     }
                 }
                 else {
+                    writer.write("<--" + level + "|" + currArr[1] + "|N|");
                 System.out.print("<--" +level + "|" + currArr[1] + "|N|");
-                for (int i = 2; i < currArr.length; i++) {
+                    for (int i = 2; i < currArr.length; i++) {
+                    writer.write(currArr[i] + " ");
                 System.out.print(currArr[i] + " ");
                 }
 
                 }
             } else if (level.equals("2")) {
                 if (!currArr[1].equals("DATE")) {
+                    writer.write("<--" + level + "|" + currArr[1] + "|N|");
                     System.out.print("<--" + level + "|" + currArr[1] + "|N|");
                     for (int i = 2; i < currArr.length; i++) {
+                        writer.write(currArr[i] + " ");
                         System.out.print(currArr[i] + " ");
                     }
                 } else {
+                    writer.write("<--" + level + "|" + currArr[1] + "|Y|");
                     System.out.print("<--" + level + "|" + currArr[1] + "|Y|");
                     for (int i = 2; i < currArr.length; i++) {
                         System.out.print(currArr[i] + " ");
+                        writer.write(currArr[i] + " ");
                     }
 
                 }
             } else {
+                writer.write("<--" + level + "|" + currArr[1] + "|N|");
                 System.out.print("<--" + level + "|" + currArr[1] + "|N|");
                 for (int i = 2; i < currArr.length; i++) {
+                    writer.write(currArr[i] + " ");
                     System.out.print(currArr[i] + " ");
                 }
             }
-
+            writer.write("\n");
             System.out.println();
         }
         sc.close();
+        writer.close();
     }
 
 }
